7b73d235fbd5e97134db90e9ec7d3180ae94af15
Kleijn95 <kleijn95@gmail.com>
2025-01-15 00:11:07 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -17,11 +17,11 @@ aggiuntaHOT();
 function toggleCards() {
   const bottone = document.querySelector("#linkHideCard");
   const allCards = document.querySelectorAll(".card");
-
   // Aggiungi l'evento click al bottone
   bottone.addEventListener("click", function () {
     for (let index = 0; index < allCards.length; index++) {
       const card = allCards[index];
+
       // Controlla se la card ha già la classe d-none
       if (card.classList.contains("d-none")) {
         card.classList.remove("d-none"); // Rendi la card visibile
@@ -30,7 +30,24 @@ function toggleCards() {
       }
     }
   });
-  console.log(`Numero totale di viaggi: ${allCards.length}`);
 }
 
 toggleCards();
+
+function countCards() {
+  const bottone = document.querySelector("#linkHideCard");
+  const allCards = document.querySelectorAll(".card");
+  let contatore = 0;
+  for (let i = 0; i < allCards.length; i++) {
+    const element = allCards[i];
+
+    // console.log(element);
+    // console.dir(element.classList.contains("d-md-none"));
+
+    if (window.getComputedStyle(element.parentElement).display !== "none") {
+      contatore++;
+    }
+  }
+  console.log(`Numero totale di viaggi: ${contatore}`);
+}
+countCards();
